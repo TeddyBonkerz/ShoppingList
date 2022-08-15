b91a39dd2da7f19ab9980fe6448a463728a04d11
TeddyBonkerz <radzlaif@gmail.com>
2022-08-15 17:20:17 -0400

    Create RecipeConfig.java

---
+++ src/main/java/com/shoppinglist/errand/helper/RecipeConfig.java
@@ -0,0 +1,19 @@
+package com.shoppinglist.errand.helper;
+
+import com.shoppinglist.errand.Model.Recipe;
+import com.shoppinglist.errand.Repositories.RecipeRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RecipeConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(RecipeRepository recipeRepository){
+        return args -> {
+            Recipe baby = new Recipe("IJ", "Recipe for Ijs baby over");
+            recipeRepository.save(baby);
+        };
+    }
+}
