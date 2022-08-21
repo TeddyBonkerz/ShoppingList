ff9a8fc0c47e520806f0b15e1ab576a2eeab1fb1
TeddyBonkerz <radzlaif@gmail.com>
2022-08-21 00:37:03 -0400

    update recipe annotations

--- src/main/java/com/shoppinglist/errand/Model/Recipe.java
+++ src/main/java/com/shoppinglist/errand/Model/Recipe.java
@@ -1,6 +1,8 @@
 package com.shoppinglist.errand.Model;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
@@ -10,6 +12,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table
+@Getter
+@Setter
 @NoArgsConstructor
 public class Recipe {
 
@@ -24,47 +28,5 @@ public class Recipe {
     public Recipe(String name, String description) {
         this.name = name;
         this.description = description;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    @Override
-    public String toString() {
-        return "Recipe{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", ingredients=" + ingredients +
-                '}';
     }
 }
--- src/main/java/com/shoppinglist/errand/Repositories/RecipeRepository.java
+++ src/main/java/com/shoppinglist/errand/Repositories/RecipeRepository.java
@@ -2,9 +2,11 @@ package com.shoppinglist.errand.Repositories;
 
 import com.shoppinglist.errand.Model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, String> {
 
     @Override
