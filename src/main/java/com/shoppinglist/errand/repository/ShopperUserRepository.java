de8d8b9cb556ca3a45f5de671127cab9ae19351b
TeddyBonkerz <radzlaif@gmail.com>
2022-08-21 21:26:39 -0400

    user enable after confirmation

--- src/main/java/com/shoppinglist/errand/repository/ShopperUserRepository.java
+++ src/main/java/com/shoppinglist/errand/repository/ShopperUserRepository.java
@@ -2,6 +2,8 @@ package com.shoppinglist.errand.repository;
 
 import com.shoppinglist.errand.model.ShopperUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,4 +14,10 @@ import java.util.Optional;
 public interface ShopperUserRepository extends JpaRepository<ShopperUser, String> {
 
     Optional<ShopperUser> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE ShopperUser user " +
+            "SET user.isEnabled = TRUE WHERE user.email = ?1")
+    void enableAppUser(String email);
 }
--- src/main/java/com/shoppinglist/errand/service/ShopperUserService.java
+++ src/main/java/com/shoppinglist/errand/service/ShopperUserService.java
@@ -1,6 +1,8 @@
 package com.shoppinglist.errand.service;
 
 import com.shoppinglist.errand.model.ShopperUser;
+import com.shoppinglist.errand.registration.ConfirmationTokenRepository;
+import com.shoppinglist.errand.registration.model.ConfirmationToken;
 import com.shoppinglist.errand.repository.ShopperUserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,12 +11,16 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Service
 @AllArgsConstructor
 public class ShopperUserService implements UserDetailsService {
 
     private final static String USER_NOT_FOUND = "user with email %s not found";
     private final ShopperUserRepository userRepository;
+    private final ConfirmationTokenRepository confirmationTokenRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -34,6 +40,16 @@ public class ShopperUserService implements UserDetailsService {
 
         userRepository.save(user);
 
-        return "YAY";
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token, LocalDateTime.now(), LocalDateTime.now().plusMinutes(15), user);
+
+        confirmationTokenRepository.save(confirmationToken);
+
+        return token;
+    }
+
+    public void enableUser(String email) {
+        userRepository.enableAppUser(email);
     }
 }
